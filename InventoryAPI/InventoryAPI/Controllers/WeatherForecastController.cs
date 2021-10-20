78d5790a0edffd2c907c5afbc0ae86159250bf7a
Osman Alhassan <oalhassan847@gmail.com>
2021-10-20 16:59:06 +0000

    Add files via upload

--- InventoryAPI/InventoryAPI/Controllers/WeatherForecastController.cs
+++ InventoryAPI/InventoryAPI/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InventoryAPI.Controllers
+namespace inventoryAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
