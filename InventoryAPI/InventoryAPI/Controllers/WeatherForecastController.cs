1dac6918898aec030aaa44cebd0dc658431d9e3b
Nicholas Gallimore <nicholas.gallimore@rovisys.com>
2021-10-20 13:28:44 -0400

    Add back in Swagger

--- InventoryAPI/InventoryAPI/Controllers/WeatherForecastController.cs
+++ InventoryAPI/InventoryAPI/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using InventoryAPI;
 
 namespace inventoryAPI.Controllers
 {
