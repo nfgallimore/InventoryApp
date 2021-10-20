a3ea43fe2ef4cf154728dce21499060d0fa8e746
Nicholas Gallimore <nicholas.gallimore@rovisys.com>
2021-10-20 19:00:54 -0400

    Add sidenav

--- InventoryApp/src/app/app.module.ts
+++ InventoryApp/src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
 import { OrdersComponent } from './shared/components/orders/orders.component';
-import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InventoryComponent,
-    OrdersComponent
+    OrdersComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    MatTableModule,
+    MaterialModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
---
+++ InventoryApp/src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,28 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnDestroy {
+
+  mobileQuery: MediaQueryList;
+
+  pages = ['Orders', 'Items'];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
