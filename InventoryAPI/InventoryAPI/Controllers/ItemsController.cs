83c7553ff5d7ccd6eec1b1e2270e30855051a88c
flemton <oalhassan847@gmail.com>
2021-10-20 19:17:08 +0000

    Add data layer and controller for items

---
+++ InventoryAPI/InventoryAPI/Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using InventoryAPI.Interfaces;
+using InventoryAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace InventoryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : Controller
+    {
+        private readonly IItemsRepository _itemsRepository;
+
+        public ItemsController(IItemsRepository itemsRepository)
+        {
+            _itemsRepository = itemsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Item> items = _itemsRepository.GetItems();
+            return Ok(items);
+        }
+    }
+}
---
+++ InventoryAPI/InventoryAPI/Repositories/ItemsRepository.cs
@@ -0,0 +1,22 @@
+﻿using InventoryAPI.Interfaces;
+using InventoryAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryAPI.Repositories
+{
+    public class ItemsRepository : IItemsRepository
+    {
+        private readonly InventoryDBContext _context;
+
+        public ItemsRepository(InventoryDBContext context)
+        {
+            _context = context;
+        }
+        public List<Item> GetItems()
+        {
+            return _context.Items.ToList();
+        }
+
+    }
+}
