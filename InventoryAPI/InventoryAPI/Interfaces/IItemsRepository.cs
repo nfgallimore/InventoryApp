18e8726e24c08b0a386802a50d9b8c996da5f507
flemton <oalhassan847@gmail.com>
2021-10-21 02:06:24 +0000

    Add rest of CRUD item endpoints

--- InventoryAPI/InventoryAPI/Interfaces/IItemsRepository.cs
+++ InventoryAPI/InventoryAPI/Interfaces/IItemsRepository.cs
@@ -6,5 +6,15 @@ namespace InventoryAPI.Interfaces
     public interface IItemsRepository
     {
         List<Item> GetItems();
+
+        Item GetItem(int id);
+
+        List<Item> GetStockItems(string name);
+
+        int CreateItem(Item item);
+
+        Item UpdateItem(Item item);
+
+        void DeleteItem(int id);
     }
 }
