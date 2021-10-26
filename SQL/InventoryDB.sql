e7ec2db4370a178f4f92bf178d8b37e827c0f173
flemton <oalhassan847@gmail.com>
2021-10-26 23:12:23 +0000

    Minor changes to InventoryDB

--- SQL/InventoryDB.sql
+++ SQL/InventoryDB.sql
@@ -25,15 +25,15 @@ DROP TABLE IF EXISTS `items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `items` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `brand` varchar(45) NOT NULL,
   `model` varchar(45) DEFAULT NULL,
-  `price` int DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
-  `total` int DEFAULT NULL,
+  `price` bigint DEFAULT NULL,
+  `quantity` bigint DEFAULT NULL,
+  `total` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (1,'Laptop','Dell','Latitude 7400 i7',1300,350,455000),(2,'Laptop','Dell','Inspiron 3593 i5',813,350,284550),(3,'Laptop','Dell','Vostro 3501 i3',421,350,147350),(4,'Laptop','Dell','XPS 13 i5',750,350,262500),(5,'Laptop','Dell','XPS 13 Touch i5',950,350,332500),(6,'Laptop','Dell','XPS 13 2-in-1 i5',1170,350,409500),(7,'Laptop','Dell','Alienware m15 R6 i7',1380,350,483000),(8,'Laptop','Dell','Alienware x15 i9',2100,350,735000),(9,'Laptop','Dell','Alienware x17 i9',2200,350,770000),(10,'Laptop','HP','Pavilion x360 i7',1050,270,283500),(11,'Laptop','HP','Probook 450 G6 i5',750,270,202500),(12,'Laptop','HP','Envy 15T i7',1350,270,364500),(13,'Laptop','HP','Envy 14 i5',1100,270,297000),(14,'Laptop','HP','Pavilion 15 i7',1350,270,364500),(15,'Laptop','HP','Pavilion Aero 13 Ryzen7',1000,270,270000),(16,'Laptop','HP','Spectre x360',1500,270,405000),(17,'Laptop','Lenovo','Legion 7 Gen6 Ryzen 7',1800,160,288000),(18,'Laptop','Lenovo','Thinkpad X1 Carbon Gen9 i7',1285,160,205600),(19,'Laptop','Gigabyte','G6 GD i5',999,160,159840),(20,'Laptop','Gigabyte','Aorus 15P YD i7',2200,160,352000),(21,'Laptop','Gigabyte','Aero 15 Oled KD i7',1500,160,240000),(22,'Laptop','LG','Gram 16 i7',1550,60,93000),(25,'Desktop','Gigabyte','GA-H270-HD3',1500,50,75000);
+INSERT INTO `items` VALUES (1,'Laptop','Dell','Latitude 7400 i7',1300,350,'455000'),(2,'Laptop','Dell','Inspiron 3593 i5',813,350,'284550'),(3,'Laptop','Dell','Vostro 3501 i3',421,350,'147350'),(4,'Laptop','Dell','XPS 13 i5',750,350,'262500'),(5,'Laptop','Dell','XPS 13 Touch i5',950,350,'332500'),(6,'Laptop','Dell','XPS 13 2-in-1 i5',1170,350,'409500'),(7,'Laptop','Dell','Alienware m15 R6 i7',1380,350,'483000'),(8,'Laptop','Dell','Alienware x15 i9',2100,350,'735000'),(9,'Laptop','Dell','Alienware x17 i9',2200,350,'770000'),(10,'Laptop','HP','Pavilion x360 i7',1050,270,'283500'),(11,'Laptop','HP','Probook 450 G6 i5',750,270,'202500'),(12,'Laptop','HP','Envy 15T i7',1350,270,'364500'),(13,'Laptop','HP','Envy 14 i5',1100,270,'297000'),(14,'Laptop','HP','Pavilion 15 i7',1350,270,'364500'),(15,'Laptop','HP','Pavilion Aero 13 Ryzen7',1000,270,'270000'),(16,'Laptop','HP','Spectre x360',1500,270,'405000'),(17,'Laptop','Lenovo','Legion 7 Gen6 Ryzen 7',1800,160,'288000'),(18,'Laptop','Lenovo','Thinkpad X1 Carbon Gen9 i7',1285,160,'205600'),(19,'Laptop','Gigabyte','G6 GD i5',999,160,'159840'),(20,'Laptop','Gigabyte','Aorus 15P YD i7',2200,160,'352000'),(21,'Laptop','Gigabyte','Aero 15 Oled KD i7',1500,160,'240000'),(22,'Laptop','LG','Gram 16 i7',1550,60,'93000'),(23,'Desktop','Gigabyte','GA-H270-HD3',1500,70,'105000'),(24,'Desktop','Gigabyte','GA-H370-HD3',2132,65,NULL);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,17 +54,17 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `contact` varchar(45) DEFAULT NULL,
-  `item_id` int DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
-  `price` int DEFAULT NULL,
-  `total` int DEFAULT NULL,
+  `item_id` bigint DEFAULT NULL,
+  `quantity` bigint DEFAULT NULL,
+  `price` bigint DEFAULT NULL,
+  `total` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `item_id_idx` (`item_id`),
   CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,8 +73,35 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,'Alhassan Osman','+233554374251',25,1,1500,1500);
+INSERT INTO `orders` VALUES (1,'Alhassan Osman','+233554374251',23,1,1500,'1500'),(2,'Newton Isaac','+233502090132',8,1,2100,'2100'),(4,'Barikisu Salifu','+233248275329',6,1,3253,'32535');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
+  `contact` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -86,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-20 20:49:35
+-- Dump completed on 2021-10-26 23:10:30
